791f702068632d94e7983e75426ad4cb78dc8eef
Christopher Jones <tkcwebdev@gmail.com>
2020-11-14 22:52:16 -0800

    Saved index.js with try catch in primary route

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 
@@ -10,36 +9,34 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // Put all API endpoints under '/api'
 app.get('/sample-route', (req, res) => {
 	try {
-// Store some values and write some functions
-const name = 'Christopher Jones';
+		// Store some values and write some functions
+		const name = 'Christopher Jones';
 
-const addMiddleName = (midname = 'Kirk') => {
-	let newName = name.split(' ', 2);
-	newName.splice(1, 0, midname);
+		const addMiddleName = (midname = 'Kirk') => {
+			let newName = name.split(' ', 2);
+			newName.splice(1, 0, midname);
 
-	return newName.join().replace(/,/gi, ' ');
-};
+			return newName.join().replace(/,/gi, ' ');
+		};
 
-console.log(req.query);
-// Generate full name
-const fullname = addMiddleName(req.query.midname);
+		console.log(req.query);
+		// Generate full name
+		const fullname = addMiddleName(req.query.midname);
 
-// Return as jso object
-res.json(fullname);
+		// Return as jso object
+		res.json(fullname);
 
-// Console.log what is sent to the result
-console.log(fullname);
+		// Console.log what is sent to the result
+		console.log(fullname);
 	} catch {
 		// Return as jso object
 		res.json('There was an error running the function');
 	}
-	
-	
 });
 
 // The catchall handler for any request that doesn't match one from above and send back to React's index.html file
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 // app.use('/', routes);
