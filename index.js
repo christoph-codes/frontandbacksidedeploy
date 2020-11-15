9105bd70fdb6b0e7e72dc61e3edff99a80ed38f9
Christopher Jones <tkcwebdev@gmail.com>
2020-11-15 01:37:16 -0800

    Reconfigured server for serving static files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", `http://localhost:3000`); // update to match the domain you will make the request from
@@ -16,6 +20,10 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 app.use('/', routes);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`Backend listening on ${port}`);
